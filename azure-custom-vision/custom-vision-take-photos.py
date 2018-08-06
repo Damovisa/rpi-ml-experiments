40bd505d4bbdf52a9fbafad0db719317d86d992d
Damian Brady <info@damianbrady.com.au>
2018-08-06 16:56:20 +1000

    Initial commit for Custom Vision demo

---
+++ azure-custom-vision/custom-vision-take-photos.py
@@ -0,0 +1,76 @@
+from sense_hat import SenseHat
+from picamera import PiCamera
+from time import sleep
+
+hat = SenseHat()
+cam = PiCamera()
+
+# sense images
+G = [0,200,0]
+Y = [200,200,0]
+O = [0,0,0]
+black =  [O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O]
+
+tick =   [O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,G,
+          O,O,O,O,O,O,G,O,
+          O,O,O,O,O,G,O,O,
+          G,O,O,O,G,O,O,O,
+          O,G,O,G,O,O,O,O,
+          O,O,G,O,O,O,O,O,
+          O,O,O,O,O,O,O,O]
+
+wait =   [O,O,O,Y,Y,O,O,O,
+          O,O,Y,O,O,Y,O,O,
+          O,Y,O,O,O,O,Y,O,
+          Y,O,Y,Y,Y,Y,O,Y,
+          Y,O,Y,Y,Y,Y,O,Y,
+          O,Y,O,O,O,O,Y,O,
+          O,O,Y,O,O,Y,O,O,
+          O,O,O,Y,Y,O,O,O]
+
+cam.resolution = [227,227]
+cam.preview_alpha = 128
+cam.start_preview()
+hat.set_pixels(wait)
+sleep(2)
+hat.clear()
+
+i=0
+running = True
+while running:
+    event = hat.stick.wait_for_event()
+    if event.action == "pressed":
+        if event.direction == "up":
+            cam.capture('/home/pi/dev/ml-experiments/images/u-' + str(i) + '.jpg')
+            hat.set_pixels(tick)
+            sleep(1)
+            i = i+1
+        if event.direction == "down":
+            cam.capture('/home/pi/dev/ml-experiments/images/d-' + str(i) + '.jpg')
+            hat.set_pixels(tick)
+            sleep(1)
+            i = i+1
+        if event.direction == "right":
+            cam.capture('/home/pi/dev/ml-experiments/images/r-' + str(i) + '.jpg')
+            hat.set_pixels(tick)
+            sleep(1)
+            i = i+1
+        if event.direction == "left":
+            cam.capture('/home/pi/dev/ml-experiments/images/l-' + str(i) + '.jpg')
+            hat.set_pixels(tick)
+            sleep(1)
+            i = i+1
+        if event.direction == "middle":
+            running = False
+        hat.clear()
+    sleep(0.1)
+    
+cam.stop_preview()
---
+++ azure-custom-vision/tf-exported-custom-vision.py
@@ -0,0 +1,149 @@
+import tensorflow as tf
+import os
+from time import sleep
+from PIL import Image
+import numpy as np
+from picamera import PiCamera
+from sense_hat import SenseHat
+
+### Vars:
+
+cam = PiCamera()
+hat = SenseHat()
+modelfile = '/home/pi/dev/ml-experiments/model-2/model.pb'
+labelfile = '/home/pi/dev/ml-experiments/model-2/labels.txt'
+tempimagefile = '/home/pi/dev/ml-experiments/pic.jpg'
+G = [0, 200, 0]
+Y = [200, 200, 0]
+B = [0, 0, 200]
+O = [0, 0, 0]
+S = [200, 200, 200]
+R = [200, 0, 0]
+wait =   [O,O,S,S,S,S,O,O,
+          O,O,S,O,O,S,O,O,
+          O,O,S,O,O,S,O,O,
+          O,O,O,S,S,O,O,O,
+          O,O,O,S,S,O,O,O,
+          O,O,S,O,O,S,O,O,
+          O,O,S,Y,Y,S,O,O,
+          O,O,S,S,S,S,O,O]
+
+coin =   [O,O,O,Y,Y,O,O,O,
+          O,O,Y,Y,Y,Y,O,O,
+          O,Y,Y,Y,Y,Y,Y,O,
+          Y,Y,Y,Y,Y,Y,Y,Y,
+          Y,Y,Y,Y,Y,Y,Y,Y,
+          O,Y,Y,Y,Y,Y,Y,O,
+          O,O,Y,Y,Y,Y,O,O,
+          O,O,O,Y,Y,O,O,O]
+
+usb =    [O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O,
+          O,B,B,B,B,B,O,O,
+          O,B,B,B,B,B,S,O,
+          O,B,B,B,B,B,S,O,
+          O,B,B,B,B,B,O,O,
+          O,O,O,O,O,O,O,O,
+          O,O,O,O,O,O,O,O]
+
+both =   [O,O,O,Y,Y,O,O,O,
+          O,O,Y,Y,Y,Y,O,O,
+          O,B,B,B,B,B,Y,O,
+          Y,B,B,B,B,B,S,Y,
+          Y,B,B,B,B,B,S,Y,
+          O,B,B,B,B,B,Y,O,
+          O,O,Y,Y,Y,Y,O,O,
+          O,O,O,Y,Y,O,O,O]
+
+nope =   [O,O,R,R,R,R,O,O,
+          O,R,O,O,O,O,R,O,
+          R,O,R,O,O,O,O,R,
+          R,O,O,R,O,O,O,R,
+          R,O,O,O,R,O,O,R,
+          R,O,O,O,O,R,O,R,
+          O,R,O,O,O,O,R,O,
+          O,O,R,R,R,R,O,O]
+
+
+### Functions:
+
+def convert_to_imgarray(image):
+    # RGB -> BGR conversion is performed as well.
+    r,g,b = np.array(image).T
+    imgarray = np.array([b,g,r]).transpose()
+    return imgarray
+
+def get_prediction(image_arr):
+    ### Run TF prediction
+
+    output_layer = 'loss:0'
+    input_node = 'Placeholder:0'
+    tags = []
+
+    with tf.Session() as sess:
+        prob_tensor = sess.graph.get_tensor_by_name(output_layer)
+        predictions, = sess.run(prob_tensor, {input_node: [image_arr] })
+
+        # Print out all of the results mapping labels to probabilities,
+        #  and tag those above 0.5
+        label_index = 0
+        for p in predictions:
+            truncated_probability = np.float64(round(p,4))
+            print (labels[label_index], truncated_probability)
+            if truncated_probability > 0.5:
+                tags.append(labels[label_index])
+            label_index += 1
+    return tags
+
+# Loading
+hat.set_pixels(wait)
+
+### Load Tensorflow Model
+
+graph_def = tf.GraphDef()
+labels = []
+
+# import graph
+with tf.gfile.FastGFile(modelfile, 'rb') as f:
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+# Get list of labels
+with open(labelfile, 'rt') as lf:
+    for l in lf:
+        labels.append(l.strip())
+
+
+### Get Images
+        
+#cam.preview_alpha = 128
+cam.resolution = [227,227]
+cam.start_preview()
+sleep(2)
+hat.clear()
+# Loop, capturing images until a keypress
+
+running = True
+while running:
+    event = hat.stick.wait_for_event()
+    if event.action == "pressed":
+        if event.direction == "middle":
+            cam.capture(tempimagefile)
+            image = Image.open(tempimagefile)
+            image = convert_to_imgarray(image)
+            prediction = get_prediction(image)
+            if 'coin' in prediction and 'usb' in prediction:
+                hat.set_pixels(both)
+            elif 'coin' in prediction:
+                hat.set_pixels(coin)
+            elif 'usb' in prediction:
+                hat.set_pixels(usb)
+            else:
+                hat.set_pixels(nope)
+            sleep(2)
+        if event.direction == "left":
+            running = False
+        hat.clear()
+    sleep(0.1)
+    
+cam.stop_preview()
